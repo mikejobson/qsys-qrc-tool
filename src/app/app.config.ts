b1aabce4ee998cabc1463fbab27e2f7279445fe4
Mike Jobson <mike@ux.digital>
2025-03-06 15:51:38 +0000

    feat: add Angular Service Worker support and update index.html formatting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 // Check if we're using the GitHub Pages build
 const isGithubPages = window.location.hostname.includes('github.io') ||
@@ -13,6 +13,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(
       routes,
       ...(isGithubPages ? [withHashLocation()] : [])
-    ),
+    ), provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ]
 };
